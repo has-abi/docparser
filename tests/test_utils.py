8bf2262d8ea84400a3f50903952b4881d9981bb1
Hassan Abida <abidahass.uca@gmail.com>
2023-01-07 17:30:45 +0100

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,27 +2,27 @@ import unittest
 
 from docparser import utils
 
+FILE_NAME_NO_PATH = "docx_example.docx"
+FILE_NAME_WITH_PATH = "/tests/docx_example.docx"
+
 
 class TestDocParser(unittest.TestCase):
-    FILE_NAME_NO_PATH = "docx_example.docx"
-    FILE_NAME_WITH_PATH = "/tests/docx_example.docx"
-
     def test_get_file_name_no_path(self):
         self.assertEqual(
-            utils.get_file_name(__class__.FILE_NAME_NO_PATH),
-            __class__.FILE_NAME_NO_PATH,
+            utils.get_file_name(FILE_NAME_NO_PATH),
+            FILE_NAME_NO_PATH,
         )
 
     def test_get_file_name_with_path(self):
         self.assertEqual(
-            utils.get_file_name(__class__.FILE_NAME_WITH_PATH),
-            __class__.FILE_NAME_NO_PATH,
+            utils.get_file_name(FILE_NAME_WITH_PATH),
+            FILE_NAME_NO_PATH,
         )
 
     def test_get_file_name_and_ext(self):
         self.assertEqual(
-            utils.get_file_name_and_ext(__class__.FILE_NAME_WITH_PATH),
-            (__class__.FILE_NAME_NO_PATH, "docx"),
+            utils.get_file_name_and_ext(FILE_NAME_WITH_PATH),
+            (FILE_NAME_NO_PATH, "docx"),
         )
 
 
