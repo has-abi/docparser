a1368cd3df4e0bf83b609991997295a5b2dbb3b6
Hassan Abida <abidahass.uca@gmail.com>
2023-01-07 17:49:27 +0100

    Update the parsing process

--- docparser/__init__.py
+++ docparser/__init__.py
@@ -5,10 +5,14 @@ from docparser.document import Document
 from docparser.parser import Parser
 from docparser.reader import Reader
 from docparser.utils import get_file_name_and_ext
+from docparser.xml_parser import XMLParser
 
 
 def parse(input_file: Union[str, BufferedReader]) -> Document:
     file_name, file_ext = get_file_name_and_ext(input_file)
     reader = Reader(input_file, file_ext)
-    parser = Parser(reader, file_ext, file_name)
+    file_parser = XMLParser(reader.zip_file, file_ext)
+    parser = Parser(file_parser, file_ext, file_name)
+    reader.zip_file.close()
+    reader.clean_up()
     return parser.document
--- docparser/parser.py
+++ docparser/parser.py
@@ -1,16 +1,16 @@
+from typing import Any
+
 from docparser.document import Document
 from docparser.reader import Reader
-from docparser.xml_parser import XMLParser
 
 
 class Parser:
-    def __init__(self, reader: Reader, file_ext: str, file_name: str) -> None:
-        self.xml_parser = XMLParser(reader.zip_file, file_ext)
+    def __init__(self, file_parser: Any, file_ext: str, file_name: str) -> None:
+        self.file_parser = file_parser
         self.document = self.get_document(file_ext, file_name)
-        self.__end(reader)
 
     def get_document(self, file_ext: str, file_name: str) -> Document:
-        splitted_content = self.xml_parser.extract_text()
+        splitted_content = self.file_parser.extract_text()
         content = " ".join(list(splitted_content.values()))
         return Document(
             name=file_name,
@@ -18,7 +18,3 @@ class Parser:
             content=content,
             splitted_content=splitted_content,
         )
-
-    def __end(self, reader: Reader) -> None:
-        reader.zip_file.close()
-        reader.clean_up()
