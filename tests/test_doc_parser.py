f7689c28bbe4cff67d6d6b5d1968a7e1e8eee0fb
Hassan Abida <abidahass.uca@gmail.com>
2023-01-08 15:33:57 +0100

    Add code coverage

--- tests/test_doc_parser.py
+++ tests/test_doc_parser.py
@@ -1,15 +1,18 @@
 import unittest
+from pathlib import Path
 
 from docparser import parse
+
+DOCX_FILE_PATH = Path(__file__).parent / "data" / "docx_example.docx"
 
 
 class TestDocParser(unittest.TestCase):
     def test_parse_docx_file_str(self):
-        document = parse("tests/data/docx_example.docx")
+        document = parse(str(DOCX_FILE_PATH))
         self.assertTrue(document.content)
 
     def test_parse_docx_file_binary(self):
-        with open("tests/data/docx_example.docx", "rb") as docx_file:
+        with open(DOCX_FILE_PATH, "rb") as docx_file:
             document = parse(docx_file)
             self.assertTrue(document.content)
 
